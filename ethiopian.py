3ec9c4b400c563f26053e114e7bfe189c86407a3
kji-dec <dec22sag@gmail.com>
2023-01-12 09:51:34 +0900

    feat: Create ethiopian multiply
    in division function, it divides input with 2.
    in multiplication function, it multiplies input with 2.
    in is_odd function, it returns if the input integer is odd or not.
    in main function, it all executes the functions.

--- ethiopian.py
+++ ethiopian.py
@@ -1,5 +1,40 @@
 def hello():
     print('Good luck!!')
 
+
+def division(div_num: int) -> (int, int):
+    return div_num // 2, div_num % 2
+
+
+def multiplication(mul_num: int) -> int:
+    return mul_num * 2
+
+
+def is_odd(quotient: int) -> bool:
+    return quotient % 2 == 1
+
+
+def main():
+    num1, num2 = map(int, input("input two integers with sapcing.").split())
+    sum_ = 0
+    div = max(num1, num2)
+    mul = min(num1, num2)
+
+    while div >= 1:
+        div, r = division(div)
+        mul = multiplication(mul)
+
+        if is_odd(div):
+            sum_ += mul
+        else:
+            continue
+
+
+    print(sum_)
+
+
+
+
+
 if __name__=='__main__':
-    hello()
+    main()
